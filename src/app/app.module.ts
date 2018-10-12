d008930308f8d0dc07d04cef5e34bbed8034ab27
casher0009 <the.casher.uber@gmail.com>
2018-10-12 10:58:29 -0500

    commit de revision para hacktoberfest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { DetailsComponent } from './details/details.component';
-import { PeopleComponent } from './people/people.component';
-import { StarshipsComponent } from './starships/starships.component';
-import { PlanetsComponent } from './planets/planets.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { DetailsComponent } from "./details/details.component";
+import { PeopleComponent } from "./people/people.component";
+import { StarshipsComponent } from "./starships/starships.component";
+import { PlanetsComponent } from "./planets/planets.component";
 
 @NgModule({
   declarations: [
@@ -19,12 +19,8 @@ import { PlanetsComponent } from './planets/planets.component';
     StarshipsComponent,
     PlanetsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule, 
-    HttpClientModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
